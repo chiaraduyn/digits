00acfb4fa82fc14065b0842561b75f27080fb716
Chiara Duyn <chiaraduyn@gmail.com>
2024-11-03 21:59:32 -1000

    Round 1

---
+++ src/components/ContactCard.tsx
@@ -0,0 +1,38 @@
+// src/components/ContactCard.tsx
+
+'use client';
+
+import { Contact } from '@/lib/validationSchemas';
+import { Card, Image } from 'react-bootstrap';
+
+interface ContactCardProps {
+  contact: Contact;
+}
+
+const ContactCard = ({ contact }: ContactCardProps) => (
+  <Card className="h-100">
+    <Card.Header className="d-flex align-items-center">
+      <Image
+        src={contact.image}
+        alt={`${contact.firstName} ${contact.lastName}`}
+        width={75}
+        height={75}
+        roundedCircle
+        className="me-3"
+      />
+      <div>
+        <Card.Title>
+          {contact.firstName}
+          {' '}
+          {contact.lastName}
+        </Card.Title>
+        <Card.Subtitle className="text-muted">{contact.address}</Card.Subtitle>
+      </div>
+    </Card.Header>
+    <Card.Body>
+      <Card.Text>{contact.description}</Card.Text>
+    </Card.Body>
+  </Card>
+);
+
+export default ContactCard;
